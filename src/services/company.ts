b896c635bd11a2c87768b9be0c71eab64edd9f3d
Le Vo Hoang Phi <hoangphi@HoangPhi.local>
2023-02-15 18:08:21 +0700

    update code

--- src/services/company.ts
+++
@@ -1,9 +0,0 @@
-import axiosInstance from 'services/axios'
-const companyApi = {
-  getInfoCompany() {
-    const url = '/users/company'
-    return axiosInstance.get(url)
-  }
-}
-
-export default companyApi
